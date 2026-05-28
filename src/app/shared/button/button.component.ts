0840cc872065ff6649fd77915e6529c2027f5067
Haren-Sharma <harensharma33@gmail.com>
2026-05-28 10:59:30 +0530

    Extending built in component behaviou via attribute selector

---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'button[app-button]',//only matches a <button> tag with appButton
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+}
